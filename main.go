dc09497c5f423abab17c5df227656c820ea205d3
Thiago <ticolls@hotmail.com>
2023-09-16 23:12:58 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/Ticolls/gopportunities/router"
+import (
+	"github.com/Ticolls/gopportunities/config"
+	"github.com/Ticolls/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
 
+	logger := *config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize router
 	router.Initialize()
 }
