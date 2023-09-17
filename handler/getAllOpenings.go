297519a7f25867b0554b8be5603a39c836f9257f
Thiago <ticolls@hotmail.com>
2023-09-17 15:40:01 -0300

    feat: add get all openings handler logic

--- handler/getAllOpenings.go
+++ handler/getAllOpenings.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Ticolls/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func GetAllOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "get-all-openings", openings)
 }
