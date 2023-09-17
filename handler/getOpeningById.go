35cc6aa6135dbb3ffa462d780b39ba130d87a318
Thiago <ticolls@hotmail.com>
2023-09-17 15:40:29 -0300

    feat: add get opening by id handler logic

--- handler/getOpeningById.go
+++ handler/getOpeningById.go
@@ -1,13 +1,28 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Ticolls/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func GetOpeningByIdHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening",
-	})
+	id := ctx.Param("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "parameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	//find Opening
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "get-opening-by-id", opening)
 }
